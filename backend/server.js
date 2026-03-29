bf6f28369e4e9e9d5436361291bd87d0f92a6103
Md Arieeb Ali <arieebali@gmail.com>
2026-03-29 11:14:44 +0530

    Set up Express server with Razorpay integration

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,29 @@
+const express =require("express");
+const Razorpay= require("razorpay");
+const cors = require("cors");
 
+const app=express();
+app.use(cors());
+app.use(express.json());
+const razorpay=new Razorpay({
+  key_id:process.env.KEY_ID,
+  key_secret:process.env.KEY_SECRET
+});
+app.post("/create-order",async(req,res)=>{
+  try{
+    const order= await razorpay.orders.create({
+    amount:5000,
+      currency:"INR",
+    });
+  res.json(order);
+  }
+  catch(err){
+    res.status(500).send("error creating order");
+  }
+});
+app.get("/",(req,res)=>{
+res.send("send running");
+         });
+const port=process.env.PORT||5000;
+  app.listen(PORT,() =>console.log("server started"));
+    
